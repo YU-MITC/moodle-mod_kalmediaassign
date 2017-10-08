a3e46fe1931de7d58c454eb34b22d3130fc01dba
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2017-10-08 19:29:00 +0900

    Add files via upload

--- version.php
+++ version.php
@@ -8,7 +8,7 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILID:\mod\kalmediaassign\version.phpTY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
@@ -27,12 +27,12 @@ if (!defined('MOODLE_INTERNAL')) {
 }
 
 $plugin->component = 'mod_kalmediaassign';
-$plugin->version = 2017100704;
-$plugin->release = 'YU Kaltura Media Assignment 1.0.6';
+$plugin->version = 2017100800;
+$plugin->release = 'YU Kaltura Media Assignment 1.0.7';
 $plugin->requires = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
 $plugin->dependencies = array(
-    'local_yukaltura' => 2017100704,
-    'local_yumymedia' => 2017100704
+    'local_yukaltura' => 2017100800,
+    'local_yumymedia' => 2017100800
 );
